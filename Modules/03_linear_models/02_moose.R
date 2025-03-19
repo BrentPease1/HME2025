ac98d494358a8444242fa60de403ded22e1e0520
Brent <bspease@ncsu.edu>
2025-03-19 11:35:21 -0500

    moose categoricals

--- Modules/03_linear_models/02_moose.R
+++ Modules/03_linear_models/02_moose.R
@@ -125,19 +125,165 @@ fit.stats <- MCMCpstr(m1mcmc$samples, params = c("fit", "fit.new"), type = "chai
 T.extreme <- fit.stats$fit.new >= fit.stats$fit
 (p.val <- mean(T.extreme)) #checks out
 
-
-# 1 continuous and 1 categorical ####
+# 1 categorical ####
 moose$year <- as.factor(moose$year)
-moose$grpsizefac <- as.factor(moose$grpsize)
-
-mod2 <- glm(observed ~ voc + year, data = moose, family = binomial())
+table(moose$year)
+levels(moose$year)
+mod2 <- glm(observed ~ year, data = moose, family = binomial())
 summary(mod2)
 
-mod2 <- glm(observed ~ -1 + voc + year, data = moose, family = binomial())
-summary(mod2)
+# reference coding (effects parameterization)
+model.matrix(mod2)[c(115:117-36),]
+moose[c(115:117-36), ]
 
-mod3 <- glm(observed ~ -1 + voc*year, data = moose, family = binomial())
+ggeffects::ggeffect(mod2, 'year')
+plot(ggeffects::ggeffect(mod2, 'year'))
+
+# means parameterization
+mod3 <- glm(observed ~ -1 + year, data = moose, family = binomial())
 summary(mod3)
+plot(ggeffects::ggeffect(mod3, 'year'))
 
-mod4 <- glm(observed ~ -1 + voc*grpsizefac, data = moose, family = binomial())
-summary(mod4)
+# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+# Nimble effects parameterization ####
+
+mod2_nim <- nimbleCode({
+  
+  # priors
+  B0 ~ dnorm(mean = 0, sd = 10) # baseline group mean
+  B1[1] <- 0 # 'corner constraints' for parameter identifiability
+  B1[2] ~ dnorm(mean = 0, sd = 10) # effect for group 2 (year 2006)
+  B1[3] ~ dnorm(mean = 0, sd = 10) # effect for group 3 (year 2007)
+  
+  # instead of writing out a bunch of these,
+  # for(bb in 2:3){
+  #   B1[bb] ~ dnorm(mean = 0, sd = 10)
+  # }
+  # B1[1] <- 0
+  
+  # likelihood
+  for(i in 1:nObs){
+    y[i] ~ dbern(p[i])
+    logit(p[i]) <- B0 + B1[year[i]]
+  }
+  
+})
+
+
+nimData <- list(y = moose$observed)
+nimConsts <- list(nObs = nrow(moose),
+                  year = as.numeric(moose$year)) # consecutive integers!!
+
+nimInits <- list(B0 = rnorm(1,0,10),
+                 B1 = c(NA, rep(rnorm(1,0,10),2))) # important
+# monitors
+keepers <- c('B0', 'B1')
+
+# MCMC Settings
+nc = 3
+nb = 2000
+ni = 10000 + nb
+nt = 1
+
+# get posteriors
+mod2mcmc <- nimbleMCMC(code = mod2_nim,
+                     constants = nimConsts,
+                     data = nimData,
+                     inits = nimInits,
+                     monitors = keepers,
+                     niter = ni,
+                     nburnin = nb,
+                     thin = nt,
+                     nchains = nc,
+                     summary = F,
+                     WAIC =F)
+
+# look at output
+MCMCtrace(object = mod2mcmc,
+          pdf = FALSE, # no export to PDF
+          ind = TRUE, # separate density lines per chain
+          params = keepers)
+MCMCsummary(object = mod2mcmc, round = 2, params = keepers)
+
+
+MCMCplot(object = mod2mcmc, 
+         params = keepers)
+
+mod2_nim <- nimbleCode({
+  
+  # priors
+  B0 ~ dnorm(mean = 0, sd = 10) # baseline group mean
+  B1[1] <- 0 # 'corner constraints' for parameter identifiability
+  B1[2] ~ dnorm(mean = 0, sd = 10) # effect for group 2 (year 2006)
+  B1[3] ~ dnorm(mean = 0, sd = 10) # effect for group 3 (year 2007)
+  
+  
+  # likelihood
+  for(i in 1:nObs){
+    y[i] ~ dbern(p[i])
+    logit(p[i]) <- B0 + B1[year[i]]
+  }
+  
+  p1 <- ilogit(B0)             # Probability for reference group
+  p2 <- ilogit(B0 + B1[2])  # Probability for group 2
+  p3 <- ilogit(B0 + B1[3])  # Probability for group 3
+  
+})
+
+keepers <- c('p1', 'p2', 'p3')
+
+mod2mcmc <- nimbleMCMC(code = mod2_nim,
+                       constants = nimConsts,
+                       data = nimData,
+                       inits = nimInits,
+                       monitors = keepers,
+                       niter = ni,
+                       nburnin = nb,
+                       thin = nt,
+                       nchains = nc,
+                       summary = F,
+                       WAIC =F)
+
+MCMCplot(object = mod2mcmc, 
+         params = keepers, 
+         horiz = F, 
+         labels = c('2005', '2006', '2007'), 
+         ylab = 'Prob. of Detection')
+
+
+# Nimble MEANS parameterization ####
+mod3_nim <- nimbleCode({
+  
+  # priors
+  for(bb in 1:3){
+    B0[bb] ~ dnorm(mean = 0, sd = 10)
+  }
+
+  
+  # likelihood
+  for(i in 1:nObs){
+    y[i] ~ dbern(p[i])
+    logit(p[i]) <- B0[year[i]]
+  }
+  
+  p1 <- ilogit(B0[1])             # Probability for reference group
+  p2 <- ilogit(B0[2])  # Probability for group 2
+  p3 <- ilogit(B0[3])  # Probability for group 3
+  
+})
+
+keepers <- c('B0', 'p1', 'p2', 'p3')
+nimInits <- list(B0 = rnorm(3,0,10)) # important
+mod3mcmc <- nimbleMCMC(code = mod3_nim,
+                       constants = nimConsts,
+                       data = nimData,
+                       inits = nimInits,
+                       monitors = keepers,
+                       niter = ni,
+                       nburnin = nb,
+                       thin = nt,
+                       nchains = nc,
+                       summary = F,
+                       WAIC =F)
+MCMCsummary(object = mod3mcmc, round = 2, params = keepers)
+
